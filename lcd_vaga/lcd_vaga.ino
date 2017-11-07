adbdbd6d6ea2a6d4dd7c854738f866ce4e37366a
SILVIA REGINA COELHO DA ROSA <44080247249@jpmorgan.sp>
2017-11-07 19:44:53 -0200

    lcd vaga maq mateus

--- lcd_vaga/lcd_vaga.ino
+++ lcd_vaga/lcd_vaga.ino
@@ -2,11 +2,30 @@
 #include <UIPEthernet.h>
 #include <utility/logging.h>
 #include <PubSubClient.h>
+#include <LiquidCrystal.h>
 
 byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xF1, 0x77 };
+int vagas_disponiveis = 0;
+int estadoVagas = 0;
+//MENU LCD
+
+const int rs = 9, en = 8, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void callback(char* topic, byte* payload, unsigned int length) {
+  int c = atoi(payload);
   Serial.println(topic);
+  Serial.println(c);
+  if (c == 0) {
+    vagas_disponiveis--;
+    
+  }
+  if (c == 1) {
+    vagas_disponiveis++;
+  }
+
+  
+
 }
 
 EthernetClient ethClient;
@@ -18,7 +37,6 @@ boolean reconnect() {
   Serial.println("reconectando...");
   if (client.connect("projeto2", "arduino", "123")) {
     Serial.println("conectado");
-    // client.publish("vaga/1","hello world");
     client.subscribe("vaga/#");
   }
   return client.connected();
@@ -27,10 +45,13 @@ boolean reconnect() {
 void setup()
 {
   Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 1);
+  lcd.print("oi");
+  
   Serial.println("iniciando...");
   Ethernet.begin(mac);
   delay(1500);
-  
   lastReconnectAttempt = 0;
 }
 
@@ -40,15 +61,28 @@ void loop()
   if (!client.connected()) {
     long now = millis();
     if (now - lastReconnectAttempt > 5000) {
-      Serial.println("reconectando...");
       lastReconnectAttempt = now;
-      
+
       if (reconnect()) {
         lastReconnectAttempt = 0;
       }
     }
   } else {
     client.loop();
+    //Serial.println(vagas_disponiveis);
+    
+
+    if (estadoVagas != vagas_disponiveis) {
+      estadoVagas = vagas_disponiveis;
+      
+      lcd.setCursor(0, 1);
+      lcd.print("              ");
+      
+      lcd.setCursor(0, 1);
+      lcd.print(vagas_disponiveis);
+    }
+
+    //delay(300);
   }
 
 }
